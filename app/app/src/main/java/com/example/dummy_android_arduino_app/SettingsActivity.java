c95ab973bfa6cb6c0604d45c6569b9e25fd0986d
kelchtermans <kkelchtermans@gmail.com>
2020-12-10 22:33:14 +0100

    add button and print yolo

--- app/app/src/main/java/com/example/dummy_android_arduino_app/SettingsActivity.java
+++ app/app/src/main/java/com/example/dummy_android_arduino_app/SettingsActivity.java
@@ -1,12 +1,16 @@
 package com.example.dummy_android_arduino_app;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,15 @@ public class SettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+        mButton = findViewById(R.id.button_send);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                // Do something
+                System.out.println("YOLO");
+            }
+        });
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
@@ -30,4 +43,14 @@ public class SettingsActivity extends AppCompatActivity {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
         }
     }
+
+    /** Called when the user touches the button */
+    public void sendMessage(View view)
+    {
+        // Do something in response to button click
+        System.out.println("YOLO");
+    }
 }
+
+
+
