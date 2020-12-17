436946115ccaa171dda0fc2a13e099e48dddb2f9
Robbe <robbe@v-b.be>
2020-12-17 21:59:14 +0100

    Request permission and Sens sms buttons and methods

--- app/app/src/main/java/com/example/dummy_android_arduino_app/SettingsActivity.java
+++ app/app/src/main/java/com/example/dummy_android_arduino_app/SettingsActivity.java
@@ -1,14 +1,62 @@
 package com.example.dummy_android_arduino_app;
 
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_SMS = 0;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        // BEGIN_INCLUDE(onRequestPermissionsResult)
+        if (requestCode == PERMISSION_REQUEST_SMS) {
+            // Request for camera permission.
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getApplicationContext(), "Permission granted", Toast.LENGTH_LONG).show();
+            } else {
+                // Permission request was denied.
+                Toast.makeText(getApplicationContext(), "Permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+        // END_INCLUDE(onRequestPermissionsResult)
+    }
+
+    public void sendSms(View view)
+    {
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED){
+            Intent intent=new Intent(getApplicationContext(),SettingsActivity.class);
+            PendingIntent pi=PendingIntent.getActivity(getApplicationContext(), 0, intent,0);
+            String message = "HET IS GELUKT";
+
+            //Get the SmsManager instance and call the sendTextMessage method to send message
+            SmsManager sms=SmsManager.getDefault();
+            sms.sendTextMessage("+32485658608", null, message, pi,null);
+        } else {
+            Toast.makeText(getApplicationContext(), "Enable permission first!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void requestPermission(View view)
+    {
+        ActivityCompat.requestPermissions(SettingsActivity.this,
+                new String[]{Manifest.permission.SEND_SMS},
+                PERMISSION_REQUEST_SMS);
+    }
 
     private Button mButton;
 
