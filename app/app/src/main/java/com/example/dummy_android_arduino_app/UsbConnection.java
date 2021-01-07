9d5fa95b9773fd544a02d9c266bb832c7fca2006
kelchtermans <kkelchtermans@gmail.com>
2021-01-07 22:16:56 +0100

    comment toast message

--- app/app/src/main/java/com/example/dummy_android_arduino_app/UsbConnection.java
+++ app/app/src/main/java/com/example/dummy_android_arduino_app/UsbConnection.java
@@ -175,8 +175,8 @@ public class UsbConnection {
                 new Intent("usb.data_received")
                         .putExtra("from", "usb")
                         .putExtra("data", data));
-//        Toast.makeText(this.context, "Received " + data,
-//                Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this.context, "Received ",
+        //        Toast.LENGTH_SHORT).show();
     }
     public void stopUsbConnection() {
         try {
