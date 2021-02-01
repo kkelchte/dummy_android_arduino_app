2ce98b1558529c70ebd8c89b739462ae748638de
kelchtermans <kkelchtermans@gmail.com>
2021-02-01 22:31:19 +0100

    created buttons and removed baudrate

--- app/app/src/main/java/com/example/dummy_android_arduino_app/SettingsActivity.java
+++ app/app/src/main/java/com/example/dummy_android_arduino_app/SettingsActivity.java
@@ -5,32 +5,27 @@ import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.widget.SwitchCompat;
 
-import java.util.Arrays;
-
-public class SettingsActivity extends Activity implements AdapterView.OnItemSelectedListener {
-    Button hButton;
-    Button lButton;
+public class SettingsActivity extends Activity {
     private static Context mContext;
-    protected Spinner baudRateSpinner;
 
     // **** USB **** //
     protected UsbConnection usbConnection;
     protected boolean usbConnected;
-    public int[] BaudRates = {9600, 14400, 19200, 38400, 57600, 115200, 230400, 460800, 921600};
-    private int baudRate = 9600;
     protected SwitchCompat connectionSwitchCompat;
 
     public static Context getContext() {
         return mContext;
     }
+
+    // **** Steering buttons ******* //
+    Button straightButton, leftButton, rightButton;
+
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -38,20 +33,14 @@ public class SettingsActivity extends Activity implements AdapterView.OnItemSele
         super.onCreate(savedInstanceState);
         mContext = getApplicationContext();
         setContentView(R.layout.settings_activity);
-        baudRateSpinner = findViewById(R.id.baud_rate_spinner);
-        hButton = (Button) findViewById(R.id.hButton);
-        hButton.setEnabled(true);
-        lButton = (Button) findViewById(R.id.lButton);
-        lButton.setEnabled(true);
-        connectionSwitchCompat = findViewById(R.id.connection_switch);
-        baudRateSpinner.setOnItemSelectedListener(this);
-        baudRateSpinner.setSelection(Arrays.binarySearch(BaudRates, baudRate));
-        toggleConnection(true);
+        straightButton = (Button) findViewById(R.id.straightButton);
+        leftButton = (Button) findViewById(R.id.leftButton);
+        rightButton = (Button) findViewById(R.id.rightButton);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     private void connectUsb() {
-        usbConnection = new UsbConnection(this, baudRate);
+        usbConnection = new UsbConnection(this, 9600);
         usbConnected = usbConnection.startUsbConnection();
     }
 
@@ -92,37 +81,27 @@ public class SettingsActivity extends Activity implements AdapterView.OnItemSele
         }
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        if (parent == baudRateSpinner) {
-            this.baudRate = Integer.parseInt(parent.getItemAtPosition(position).toString());
-            Toast.makeText(getContext(), "Setting baudrate to " + this.baudRate,
-                    Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-        // Do nothing.
-    }
-
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    public void onClickHigh(View view) {
+    public void sendStraight(View view) {
         if (usbConnected){
-            Toast.makeText(getContext(), "Sending 6.", Toast.LENGTH_SHORT).show();
-            usbConnection.send("6");
-        } else {
-            Toast.makeText(getContext(), "Failed to send 6.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getContext(), "Sending 1.", Toast.LENGTH_SHORT).show();
+            usbConnection.send("1");
         }
     }
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    public void onClickLow(View view) {
-        if (usbConnected) {
-            Toast.makeText(getContext(), "Sending 4.", Toast.LENGTH_SHORT).show();
-            usbConnection.send("4");
-        } else {
-            Toast.makeText(getContext(), "Failed to send 4.", Toast.LENGTH_SHORT).show();
+    public void sendLeft(View view) {
+        if (usbConnected){
+            Toast.makeText(getContext(), "Sending 0.", Toast.LENGTH_SHORT).show();
+            usbConnection.send("0");
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void sendRight(View view) {
+        if (usbConnected){
+            Toast.makeText(getContext(), "Sending 2.", Toast.LENGTH_SHORT).show();
+            usbConnection.send("2");
         }
     }
 
